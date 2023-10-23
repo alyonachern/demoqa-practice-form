e5bbde7982444542a00e7dc51c5bb264fde9c506
alyonachern <alenaisakova@mail,ru>
2023-10-23 22:50:46 +0300

    changed build.gradle and TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,16 +16,13 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
 
-        String browser = System.getProperty("browser", "chrome");
-        String version = System.getProperty("version", "101");
-        String browserSize = System.getProperty("windowSize", "1920x1080");
-
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = browserSize;
-        Configuration.browser = browser;
-        Configuration.browserVersion = version;
+        Configuration.browserSize = System.getProperty("windowSize", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "101");
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = false;
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
